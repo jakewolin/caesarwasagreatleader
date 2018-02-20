d7d12ec89c837079d4707c4d061a81e998e4a34b
Jake Parsons <32745906+jakewolin@users.noreply.github.com>
2018-02-20 13:09:36 -0500

    Add files via upload

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,8 +1,28 @@
 def shift(letter, shift_amount):
-    unicode_value=ord(letter)+shift_amount\
-                   
-    new_letter=chr(unicode_value)
+
+    value=ord(letter)+shift_amount
+
+    while value>126:
+        value-=95 
+                  
+    new_letter=chr(value)
     return new_letter
+
+#--------------------------------------
+
+def shift2(letter, shift_amount):
+
+    value=ord(letter)-shift_amount
+
+    while value<32:
+        value+=95
+
+    #print(value)
+
+    new_letter=chr(value)                 
+    return new_letter
+
+#--------------------------------------
 
 def encrypt(message, shift_amount):
     result=""
@@ -11,19 +31,22 @@ def encrypt(message, shift_amount):
         result+=shift(letter, shift_amount)
     return result
 
+#--------------------------------------
 
 def decrypt(message, shift_amount):
     result=""
 
     for letter in message:
-        result+=shift(letter, -shift_amount)
+        result+=shift2(letter, shift_amount)
+        #print(shift2(letter, shift_amount))
     return result
 
-secret_message = "encryption is fun"
-encrypted_message = encrypt(secret_message, 3)
-decrypted_message = decrypt(encrypted_message, 3)
+#--------------------------------------
+
+secret_message = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ 123!@#"
+encrypted_message = encrypt(secret_message, 4779)
+decrypted_message = decrypt(encrypted_message, 4779)
 print(secret_message)
 print(encrypted_message)
 print(decrypted_message)
-
 
